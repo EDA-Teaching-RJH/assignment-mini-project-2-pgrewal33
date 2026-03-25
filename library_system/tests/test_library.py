c10b1ad8e4060c180269bb4a035e26885bb9b6f2
pgrewal33 <pg10@kent.ac.uk>
2026-03-25 22:01:47 +0000

    added tests for correct and incorrect isbn formats

--- library_system/tests/test_library.py
+++ library_system/tests/test_library.py
@@ -9,4 +9,12 @@ from models.library import Library
 from utilities.functions import validate_isbn, validate_year
 
 class TestRegex(unittest.TestCase):
-    
+    def test_valid_isbn(self):
+        self.assertTrue(validate_isbn("123-456-789"))
+        self.assertTrue(validate_isbn("987-123-456"))
+
+    def test_invalid_isbn(self):
+        self.assertTrue(validate_isbn("123456789"))
+        self.assertTrue(validate_isbn("12-34-56-78"))
+        self.assertTrue(validate_isbn("qwe-rty-uio"))
+        
