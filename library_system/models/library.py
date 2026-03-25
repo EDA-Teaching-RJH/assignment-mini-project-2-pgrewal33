cb6035b30efdbcaaae09f26cf2e4f7a7425d64eb
pgrewal33 <pg10@kent.ac.uk>
2026-03-25 15:43:43 +0000

    so that when a book is added its not a duplicate made sure it checks against all isbns in the list

--- library_system/models/library.py
+++ library_system/models/library.py
@@ -2,4 +2,11 @@ class library:
 
     def __init__(self):
         self.books = []
-        
+
+    def add_book(self, book):
+        for existing in self.books:
+            if existing.isbn == book.isbn:
+                return False
+        self.books.append(books)
+        return True
+
